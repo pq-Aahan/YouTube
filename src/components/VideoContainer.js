ec8f920b42446d35473515faa8255a55edcc1f52
pq-Aahan <pulast.aahan@gmail.com>
2024-11-15 23:22:30 +0530

    commit

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { YOUTUBE_VIDEOS_API } from '../utils/constants';
 
 const VideoContainer = () => {
+
+  const[video, setVideos]=useState([]);
+
+  useEffect(()=>{
+   getVideos();
+  },[]);
+
+
+const getVideos=async()=>{
+const data=await fetch(YOUTUBE_VIDEOS_API);
+const json=await data.json();
+console.log(json);
+setVideos(json.items);
+}
+
+
   return (
     <div>
       Video Container
---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+  const API_KEY= "AIzaSyCvOc7_Fy2RO-t2hyNQ-43pPIoask8GZ0I"
+
+export const YOUTUBE_VIDEOS_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key="+ API_KEY;
+
+
