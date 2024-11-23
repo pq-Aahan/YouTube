0a0a699d184f2cde9a7569294472038e52c38bc0
pq-Aahan <pulast.aahan@gmail.com>
2024-11-23 11:41:02 +0530

    commit

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,28 +1,48 @@
-import React, { useEffect } from 'react'
-import ChatMessage from './ChatMessage'
-import { useDispatch } from 'react-redux'
-import { addMessage } from '../utils/chatSlice';
+import React, { useEffect, useState } from "react";
+import ChatMessage from "./ChatMessage";
+import { useDispatch, useSelector } from "react-redux";
+import { addMessage } from "../utils/chatSlice";
 
 const LiveChat = () => {
 
- const dispatch=useDispatch();
+const[liveMessage, setLiveMessage]=useState("");
 
-    useEffect(()=>{
-        const i=setInterval(()=>{
-    //Api polling
-    dispatch(addMessage({
-        name:"Aahan",
-        message:"Lorem ipsum dolor",
-    }))
-        },2000);
-        return ()=>clearInterval(i)
-    },[])
+  const dispatch = useDispatch();
+  const chatMessages = useSelector((store) => store.chat?.messages || []);
+
+  useEffect(() => {
+    const i = setInterval(() => {
+      // Simulate API polling
+      dispatch(
+        addMessage({
+          name: "Aahan",
+          message: "Lorem ipsum dolor",
+        })
+      );
+    }, 2000);
+    return () => clearInterval(i);
+  }, [dispatch]);
 
   return (
-    <div className='w-full h-[600px] ml-2 p-2 border border-black bg-slate-200 rounded-lg'>
-      <ChatMessage name="Aahan" message="This is youtube with react"/>
+    <>
+    <div className="w-full h-[600px] ml-2 p-2 border border-black bg-slate-200 rounded-lg overflow-y-scroll flex-col-reverse">
+     <div> {chatMessages.map((c, i) => (
+        <ChatMessage key={i} name={c.name} message={c.message} />
+      ))}</div>
+      
     </div>
-  )
-}
+     <form className="w-full p-2  ml-2 border border-black" onSubmit={(e)=>{e.preventDefault();
+        console.log("on form submit", liveMessage);
+        dispatch(addMessage({name:"Tukun",
+            message:liveMessage, 
+        }))
+        setLiveMessage("")
+     }}>
+        <input className="w-95 px-2 " type="text" value={liveMessage} onChange={(e)=>{setLiveMessage(e.target.value)}}/>
+        <button className="px-2 mx-2 bg-green-50">Submit</button>
+     </form>
+     </>
+  );
+};
 
-export default LiveChat
+export default LiveChat;
--- src/utils/store.js
+++ src/utils/store.js
@@ -1,14 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import appSlice from "./appSlice"; // Import the appSlice reducer
-import searchSlice from "./searchSlice"; // Import the appSlice reducer
+import searchSlice from "./searchSlice"; // Import the searchSlice reducer
+import chatSlice from "./chatSlice"; // Import the chatSlice reducer
 
-
-const store=configureStore({
-reducer:{
+const store = configureStore({
+  reducer: {
     app: appSlice,
-    search:searchSlice,
-    chat:chatSice,
-},
+    search: searchSlice,
+    chat: chatSlice, // Correctly referenced chatSlice
+  },
 });
 
 export default store;
